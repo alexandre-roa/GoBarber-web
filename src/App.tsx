b3ecbd40125172e9bb2cc2206602591f5640d4b2
alexandre-roa <alexandre.z.roa@gmail.com>
2020-09-02 12:07:39 -0300

    athentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import { AuthProvider } from './hooks/AuthContext';
 // import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
